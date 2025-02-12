be96032c3c6d3af85a729df0d5745a99d4ef322f
Ted <rozanovfedya@gmail.com>
2025-02-12 20:20:05 +0300

    add header and CMake

--- main_lab1.cpp
+++ main_lab1.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 #include <string.h>
+#include "head_lab1.hpp"
 
-int interactive();
+int interactive() {
+    std::cout << "Интерактивный режим запущен." << std::endl;
+    // Здесь можно добавить логику для интерактивного режима
+    return 0;
+}
 
-int demo();
+int demo() {
+    std::cout << "Демо-режим запущен." << std::endl;
+    // Здесь можно добавить логику для демо-режима
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
-    bool isInteractive = false;    // по умолчанию демо-режим.
+    bool isInteractive = false;    // По умолчанию демо-режим.
 
-    // Если i передается в качестве аргумента, то программу
-    // необходимо запустить в интерактивном режиме
+    // Если "i" передается в качестве аргумента, то программа переходит в интерактивный режим
     if ((argc == 2) && strcmp(argv[1], "i") == 0) {
         isInteractive = true;
     }
 
     if (isInteractive) {
         return interactive();
-
     } else {
         return demo();
     }
